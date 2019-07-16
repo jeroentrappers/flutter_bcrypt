e5455f504ead95dda993763c6ff8776eddb95340
Jeroen Trappers <jeroen.trappers@gmail.com>
2019-07-16 11:16:00 +0200

    Example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,31 +12,27 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  String _hash = 'Unknown';
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    hash();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
+  Future<void> hash() async {
+    String hash;
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
-      platformVersion = await FlutterBcrypt.hashPw(password: r'3282aae5-f104-4639-88a3-3af9a1efb069', salt: r'$2b$06$C6UzMDM.H6dfI/f/IKxGhu');
+      hash = await FlutterBcrypt.hashPw(password: r'3282aae5-f104-4639-88a3-3af9a1efb069', salt: r'$2b$06$C6UzMDM.H6dfI/f/IKxGhu');
     } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
+      hash = 'Failed to get hash.';
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
 
     setState(() {
-      _platformVersion = platformVersion;
+      _hash = hash;
     });
   }
 
@@ -45,10 +41,10 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Bcrypt plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Text('Hashed password: $_hash\n'),
         ),
       ),
     );

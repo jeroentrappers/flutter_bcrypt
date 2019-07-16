085f999842788c5b2de7369eb2a192ab9b58f74e
Jeroen Trappers <jeroen.trappers@gmail.com>
2019-07-16 17:03:30 +0200

    Format source file

--- lib/flutter_bcrypt.dart
+++ lib/flutter_bcrypt.dart
@@ -2,18 +2,21 @@ import 'package:flutter/services.dart';
 import 'package:flutter/foundation.dart';
 
 class FlutterBcrypt {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_bcrypt');
+  static const MethodChannel _channel = const MethodChannel('flutter_bcrypt');
 
-  static Future<String> hashPw({@required String password, @required String salt}) async =>
-    await _channel.invokeMethod('hashPw', {'password': password, 'salt': salt});
+  static Future<String> hashPw(
+          {@required String password, @required String salt}) async =>
+      await _channel
+          .invokeMethod('hashPw', {'password': password, 'salt': salt});
 
   static Future<String> salt() async =>
-      await _channel.invokeMethod('salt', {'rounds' : 6});
+      await _channel.invokeMethod('salt', {'rounds': 6});
 
   static Future<String> saltWithRounds({@required int rounds}) async =>
       await _channel.invokeMethod('saltWithRounds', {'rounds': rounds});
 
-  static Future<bool> verify({@required String password, @required String hash}) async =>
-      await _channel.invokeMethod('verify', {'password': password, 'hash': hash});
+  static Future<bool> verify(
+          {@required String password, @required String hash}) async =>
+      await _channel
+          .invokeMethod('verify', {'password': password, 'hash': hash});
 }

91ed91c24359a314ffc9fc6705ebf5e354f1e02b
Jeroen Trappers <jeroen.trappers@gmail.com>
2019-07-16 17:23:34 +0200

    Improve documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## 1.0.0
 
 * Initial release with support for iOS and Android
+
+## 1.0.3
+
+* Improved documentation, format health for flutter packages
--- lib/flutter_bcrypt.dart
+++ lib/flutter_bcrypt.dart
@@ -4,17 +4,28 @@ import 'package:flutter/foundation.dart';
 class FlutterBcrypt {
   static const MethodChannel _channel = const MethodChannel('flutter_bcrypt');
 
+  /// Create passworde hash for given password and salt. 
+  /// Password must be a plain UTF-8 compatible String. 
+  /// Salt must be a string in Modular Crypt Format with $ separators
+  /// for example r'$2b$06$C6UzMDM.H6dfI/f/IKxGhu'
   static Future<String> hashPw(
           {@required String password, @required String salt}) async =>
       await _channel
           .invokeMethod('hashPw', {'password': password, 'salt': salt});
 
+  /// Generate a 16 byte salt with 6 rounds of cost in Modular Crypt Format.
   static Future<String> salt() async =>
       await _channel.invokeMethod('salt', {'rounds': 6});
 
+  /// Generate a salt with a given cost. Cost must be between 4 and 31.
   static Future<String> saltWithRounds({@required int rounds}) async =>
       await _channel.invokeMethod('saltWithRounds', {'rounds': rounds});
 
+  /// Verify if the given password matches the given hash. 
+  /// Password must be a plain UTF-8 compatible String
+  /// Hash must be provided in Modular Crypt Format. Eg. 
+  /// $2y$06$doGnefu9cbLkJTn8sef7U.dynHJFe5hS6xp7vLWb2Zu7e8cOuMVmS
+  /// This includes the version, complexity and 16 bytes of salt.
   static Future<bool> verify(
           {@required String password, @required String hash}) async =>
       await _channel
